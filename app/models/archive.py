3c24562ec3f05e7ce0511380fae8205001e817d4
JackieC-12 <tealkenn@gmail.com>
2025-01-03 23:05:57 -0600

    Pushed alembic.ini to main folder and created archive migration

--- app/models/archive.py
+++ app/models/archive.py
@@ -14,5 +14,6 @@ class Archive(db.Model):
     title = db.Column(db.String(25), nullable=False)
     description = db.Column(db.String)
     time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
     # user = db.relationship("User", back_populates="archives")
     # sources = db.relationship("Sources", back_populates="archives")
---
+++ migrations/versions/20250103_230453_create_archive_table.py
@@ -0,0 +1,24 @@
+"""create archive table
+
+Revision ID: 40637580507e
+Revises: ffdc0a98111c
+Create Date: 2025-01-03 23:04:53.763746
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '40637580507e'
+down_revision = 'ffdc0a98111c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
